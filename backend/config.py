0171449409cbdd6ca8cdda2df19ca1587b61d2f1
azzedev <azzedine.dev@outlook.fr>
2024-11-15 19:03:11 +0100

    Projet final

--- backend/config.py
+++ backend/config.py
@@ -2,9 +2,12 @@ import os
 from datetime import timedelta
 from dotenv import load_dotenv
 
+
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-load_dotenv(os.path.join(basedir, ".env"))
+# Permet de charger le fichier d'environnement présent a la racine du projet
+load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
 
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -18,10 +21,13 @@ class Config(object):
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
 class DevelopmentConfig(Config):
-    # Utiliser la variable d’environnement DATABASE_URL pour se connecter à MySQL
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///" + os.path.join(basedir, "data.db"))
+    # URL de connexion pour la BDD
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://azzed:mdpdeazzed@mysql/projet_docker_bdd")
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
 
+
+# Health check pour vérifier qu'on se connecte a la BDD
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///"
+    SQLALCHEMY_DATABASE_URI = os.getenv("TEST_DATABASE_URL", "mysql+pymysql://test_user:test_password@localhost/test_projet_docker_bdd")
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=5)
